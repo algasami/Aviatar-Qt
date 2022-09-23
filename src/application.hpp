adad35aa50f41c7a081f60a650a9719e09f824da
algasami <105358814+algasami@users.noreply.github.com>
2022-09-23 20:53:53 +0800

    image views

--- src/application.hpp
+++ src/application.hpp
@@ -4,171 +4,135 @@
 //
 //  Created by algasami on 2022/9/17.
 //
-
-#define VK_USE_PLATFORM_MACOS_MVK
-#define GLFW_INCLUDE_VULKAN
-
-#include <GLFW/glfw3.h>
-#include <GLFW/glfw3native.h>
-
-#include <algorithm>
-#include <iostream>
-#include <stdexcept>
-
-#include <limits>
-#include <map>
-#include <optional>
-#include <set>
-#include <string>
-#include <vector>
-
-#include <cstdint>
-#include <cstdlib>
-#include <cstring>
-
 #include "supplement.hpp"
+#include "vulkan/vulkan_core.h"
 
 #ifndef application_h
 #define application_h
 
 // Helper
 
-struct QueueFamilyIndices
-{
-  std::optional<uint32_t> graphicsFamily;
-  std::optional<uint32_t> presentFamily;
-
-  bool isComplete()
-  {
-    return graphicsFamily.has_value() && presentFamily.has_value();
-  }
+struct SwapChainSupportDetails {
+    VkSurfaceCapabilitiesKHR capabilities;
+    std::vector<VkSurfaceFormatKHR> formats;
+    std::vector<VkPresentModeKHR> presentModes;
 };
 
-struct SwapChainSupportDetails
-{
-  VkSurfaceCapabilitiesKHR capabilities;
-  std::vector<VkSurfaceFormatKHR> formats;
-  std::vector<VkPresentModeKHR> presentModes;
-};
+class Application {
+  public:
+    Application() = default;
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
 
-class Application
-{
-public:
-  Application() = default;
-  void run()
-  {
-    initWindow();
-    initVulkan();
-    mainLoop();
-    cleanup();
-  }
+  private:
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
 
-private:
-  const uint32_t WIDTH = 800;
-  const uint32_t HEIGHT = 600;
+    // Specify enabled layers
+    const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation"};
 
-  // Specify enabled layers
-  const std::vector<const char *> validationLayers = {"VK_LAYER_KHRONOS_validation"};
-
-  // Specify device extensions // Logical
-  const std::vector<const char *> logicalDeviceExtensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+    // Specify device extensions // Logical
+    const std::vector<const char*> logicalDeviceExtensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
 
 #ifdef NDEBUG
-  static constexpr bool enableValidationLayers = false;
+    static constexpr bool enableValidationLayers = false;
 #else
-  static constexpr bool enableValidationLayers = true;
+    static constexpr bool enableValidationLayers = true;
 #endif
 
-  GLFWwindow *window;
-  VkInstance instance;
-  VkDebugUtilsMessengerEXT debugMessenger;
+    GLFWwindow* window;
+    VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
 
-  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
-  VkDevice logicalDevice = VK_NULL_HANDLE;
-  VkQueue graphicsQueue;
+    VkDevice logicalDevice = VK_NULL_HANDLE;
+    VkQueue graphicsQueue;
 
-  VkSurfaceKHR surface; ///< Interfacing with the window system
-  VkQueue presentQueue; ///< Presentation ^^
+    VkSurfaceKHR surface; ///< Interfacing with the window system
+    VkQueue presentQueue; ///< Presentation ^^
 
-  VkSwapchainKHR swapChain;
-  std::vector<VkImage> swapChainImages;
-  VkFormat swapChainImageFormat;
-  VkExtent2D swapChainExtent;
+    VkSwapchainKHR swapChain;
+    std::vector<VkImage> swapChainImages;
+    std::vector<VkImageView> swapChainImageViews;
+    VkFormat swapChainImageFormat;
+    VkExtent2D swapChainExtent;
 
-  void initWindow()
-  {
-    glfwInit();
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-  }
-
-  void initVulkan()
-  {
-    createInstance();
-    setupDebugManager();
-    createSurface();
-    pickPhysicalDevice();
-    createLogicalDevice();
-    createSwapChain();
-  }
-
-  void mainLoop()
-  {
-    while (!glfwWindowShouldClose(window))
-    {
-      glfwPollEvents();
+    void initWindow() {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
-  }
 
-  void cleanup()
-  {
-    if (enableValidationLayers)
-    {
-      DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+    void initVulkan() {
+        createInstance();
+        setupDebugManager();
+        createSurface();
+        pickPhysicalDevice();
+        createLogicalDevice();
+        createSwapChain();
+        createImageViews();
     }
-    vkDestroySwapchainKHR(logicalDevice, swapChain, nullptr);
-    vkDestroyDevice(logicalDevice, nullptr);
-    vkDestroySurfaceKHR(instance, surface, nullptr);
-    vkDestroyInstance(instance, nullptr);
-    glfwDestroyWindow(window);
 
-    glfwTerminate();
-  }
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
 
-  void createInstance();
-  bool checkValidationLayerSupport();
-  void setupDebugManager();
+    void cleanup() {
+        for (auto& imageView : swapChainImageViews) {
+            vkDestroyImageView(logicalDevice, imageView, nullptr);
+        }
+        if (enableValidationLayers) {
+            DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+        }
+        vkDestroySwapchainKHR(logicalDevice, swapChain, nullptr);
+        vkDestroyDevice(logicalDevice, nullptr);
+        vkDestroySurfaceKHR(instance, surface, nullptr);
+        vkDestroyInstance(instance, nullptr);
+        glfwDestroyWindow(window);
 
-  void pickPhysicalDevice();
-  int ratePhysicalDevice(VkPhysicalDevice device);
-  QueueFamilyIndices findPhysicalDeviceQueueFamilies(VkPhysicalDevice device);
+        glfwTerminate();
+    }
 
-  void createLogicalDevice();
+    void createInstance();
+    bool checkValidationLayerSupport();
+    void setupDebugManager();
 
-  void createSurface();
-  void createSwapChain();
+    void pickPhysicalDevice();
+    int ratePhysicalDevice(VkPhysicalDevice device);
+    QueueFamilyIndices findPhysicalDeviceQueueFamilies(VkPhysicalDevice device);
 
-  bool checkPhysDeviceExtensionSupport(VkPhysicalDevice device);
+    void createLogicalDevice();
 
-  SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+    void createSurface();
+    void createSwapChain();
 
-  VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &availableFormats);
-  VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR> &availablePresentModes);
+    void createImageViews();
 
-  VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities);
+    bool checkPhysDeviceExtensionSupport(VkPhysicalDevice device);
 
-  // Static Members
-  static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-                                                      VkDebugUtilsMessageTypeFlagsEXT messageType,
-                                                      const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData, void *pUserData);
-  static void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger,
-                                            const VkAllocationCallbacks *pAllocator);
-  static VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo,
-                                               const VkAllocationCallbacks *pAllocator, VkDebugUtilsMessengerEXT *pDebugMessenger);
-  static void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo);
-  static std::vector<const char *> getRequiredExtensions();
-  static std::vector<const char *> getLogicalDeviceRequiredExtensions(VkPhysicalDevice physicalDevice);
+    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+    VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+
+    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+
+    // Static Members
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType,
+                                                        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
+    static void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
+    static VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator,
+                                                 VkDebugUtilsMessengerEXT* pDebugMessenger);
+    static void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+    static std::vector<const char*> getRequiredExtensions();
+    static std::vector<const char*> getLogicalDeviceRequiredExtensions(VkPhysicalDevice physicalDevice);
 };
 #endif /* application_h */
--- src/supplement.hpp
+++ src/supplement.hpp
@@ -7,7 +7,31 @@
 
 #ifndef supplement_h
 #define supplement_h
+#define VK_USE_PLATFORM_MACOS_MVK
+#define GLFW_INCLUDE_VULKAN
+
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
+
+#include <limits>
+#include <map>
+#include <optional>
+#include <set>
+#include <string>
+#include <vector>
+
 #include <cstdint>
+#include <cstdlib>
+#include <cstring>
 typedef uint8_t uint2_t;
+struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+    std::optional<uint32_t> presentFamily;
+
+    bool isComplete() { return graphicsFamily.has_value() && presentFamily.has_value(); }
+};
 
 #endif
